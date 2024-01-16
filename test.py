1f6219d2ed69dd0736d4d1f7cda77e600b2cc2ce
Amska1234 <154337009+Amska1234@users.noreply.github.com>
2024-01-16 23:33:37 +0300

    test.py

--- test.py
+++ test.py
@@ -5,4 +5,6 @@ import request
 # Тест проверки создания заказа
 def test_order_creation():
     creation_response = request.post_new_order()
-    track_id = creation_response.json().get('track', None)
+    track_id = creation_response.json()['track']
+    response = request.get_orders_track(track_id)
+    assert response.status_code == 200
